34d8dd51f32ebafe81b690f206f355cd4f3fd9ad
Natalia Diaz <nataliadiazc@hotmail.com>
2023-03-23 11:52:57 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:fl_components/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      // home: const CardScreen(),
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'listview1': (context) => const Listview1Screen(),
+        'listview2': (context) => const Listview2Screen(),
+        'alert': (context) => const AlertScreen(),
+        'card': (context) => const CardScreen(),
+      },
+    );
+  }
+}
